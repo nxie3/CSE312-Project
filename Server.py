88daef1acfca9d5f197f27663d0ac2c1ca403bb7
Nicholas Xie <xien610@Nicholass-MacBook-Pro.local>
2021-12-08 21:10:42 -0500

    Slight Revision

--- Server.py
+++ Server.py
@@ -1,8 +1,8 @@
 import socketserver
 import sys
 import time
-import hashlib
-from base64 import b64encode
+import json
+import Database
 
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
@@ -27,22 +27,30 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
         data = recieved_data.decode().split("\r\n")
         temp = data[0].split()
 
-        if temp[1] == "/":
-            content = open("Homescreen.html", mode="r", encoding="utf-8")
-            content = content.read()
-            length = str(len(content.encode("utf-8")))
-            self.request.sendall(("HTTP/1.1 200 OK\r\nContent-Length: " + length + "\r\nContent-Type: text/html; charset=utf-8; X-Content-Type-Options=nosniff\r\n\r\n" + content).encode())
-        elif temp[1] == "/server.js":
-            content = open("server.js", mode="r", encoding="utf-8")
-            content = content.read()
-            length = str(len(content.encode("utf-8")))
-            self.request.sendall(("HTTP/1.1 200 OK\r\nContent-Length: " + length + "\r\nContent-Type: text/javascript; charset=utf-8; X-Content-Type-Options=nosniff\r\n\r\n" + content).encode())
-        elif temp[1] == "/registration":
-            content = open("Account_Registration.html", mode="r", encoding="utf-8")
-            content = content.read()
-            length = str(len(content.encode("utf-8")))
-            self.request.sendall(("HTTP/1.1 200 OK\r\nContent-Length: " + length + "\r\nContent-Type: text/html; charset=utf-8; X-Content-Type-Options=nosniff\r\n\r\n" + content).encode())
-
+        if temp[0] == "GET":
+            if temp[1] == "/":
+                content = open("Homescreen.html", mode="r", encoding="utf-8")
+                content = content.read()
+                length = str(len(content.encode("utf-8")))
+                self.request.sendall(("HTTP/1.1 200 OK\r\nContent-Length: " + length + "\r\nContent-Type: text/html; charset=utf-8; X-Content-Type-Options=nosniff\r\n\r\n" + content).encode())
+            elif temp[1] == "/server.js":
+                content = open("server.js", mode="r", encoding="utf-8")
+                content = content.read()
+                length = str(len(content.encode("utf-8")))
+                self.request.sendall(("HTTP/1.1 200 OK\r\nContent-Length: " + length + "\r\nContent-Type: text/javascript; charset=utf-8; X-Content-Type-Options=nosniff\r\n\r\n" + content).encode())
+            elif temp[1] == "/registration":
+                content = open("Account_Registration.html", mode="r", encoding="utf-8")
+                content = content.read()
+                length = str(len(content.encode("utf-8")))
+                self.request.sendall(("HTTP/1.1 200 OK\r\nContent-Length: " + length + "\r\nContent-Type: text/html; charset=utf-8; X-Content-Type-Options=nosniff\r\n\r\n" + content).encode())
+        elif temp[0] == "POST":
+            if temp[1] == "/registered":
+                user_info = json.loads(data[len(data)-1])
+                username = user_info['username']
+                password = user_info['password']
+                print(username, password)
+                Database.add_users(username, password)
+                self.request.sendall("HTTP/1.1 302 Moved Permanently\r\nContent-Length: 0\r\nLocation: \\ \r\n\r\n".encode())
 
 if __name__ == "__main__":
     host = "0.0.0.0"
